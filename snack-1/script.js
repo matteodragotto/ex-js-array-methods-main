ad8fa598d05f339553642e2a3fc870ce3a0b3252
matteodragotto <matteodrag8@gmail.com>
2024-12-02 13:24:40 +0100

    [feat] snack 1 fine

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,10 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+names.forEach ((name) => {
+  return console.log(name);
+  
+})
+
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
