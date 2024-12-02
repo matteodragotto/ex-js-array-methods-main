b88ad33caa7eda3e3c9b28c55ac3488416f8b6c3
matteodragotto <matteodrag8@gmail.com>
2024-12-02 13:40:19 +0100

    [feat] snack 3 fine

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const numbersPlusOne = numbers.map ((number) => {
+  return ++number
+})
+
+console.log(numbersPlusOne);
+
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
